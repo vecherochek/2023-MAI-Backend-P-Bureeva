57a038bbb00c2d92c0ed05b382fefe82be21c372
Polina Bureeva <pbureeva@gmail.com>
2023-05-11 21:57:12 +0300

    added hw_3

--- homework-03/myproj/app/urls.py
+++ homework-03/myproj/app/urls.py
@@ -14,11 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from django.contrib import admin
+
 from django.urls import path
-from django.conf.urls import include
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('app/', include('app.urls')),
+    path('planet/<int:planet_id>', views.planet, name='planet'),
+    path('moon/<int:moon_id>', views.moon, name='moon'),
+    path('systems', views.astronomical_systems, name='astronomical_systems')
 ]
